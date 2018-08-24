600991fc2faeaceb17da26437fc8f5fd244e87cf
Sergio Patrick Hirayama Trautmann <42048914+sergiotrautmann@users.noreply.github.com>
2018-08-24 12:34:50 -0300

    Update ex7.py

--- ex7.py
+++ ex7.py
@@ -12,3 +12,29 @@ arquivo.writelines(conteudo)
 arquivo.close()
 
 
+arquivo = open('abastecimento.txt','r+')
+conteudo = arquivo.readlines()
+km = input('Qual a quilometragem do veiculo na hora do abastecimento?:')
+quantidade = input('Qual a quantidade de combustivel abastecido?:')
+valor = input('Qual o valor pago?:')
+c = km + ';' + quantidade + ';' + valor +'\n'
+conteudo.append(c)
+arquivo = open('abastecimento.txt', 'r+')
+arquivo.writelines(conteudo )
+arquivo.close()
+arquivo = open('abastecimento.txt','r')
+for linha in arquivo:
+    l =linha.split(';')
+    tkm = float(l[0])
+    tqt = float(l[1])
+    tvl = float(l[2])
+    tkm1 = 0
+    tqt1 = 0
+    tvl1 = 0
+    tkm1 += + tkm
+    tqt1 += + tqt
+    tvl1 += + tvl
+    media = 0
+    media += tkm1/ tqt1
+    print('a media de consumo por km é', media)
+    print('o total gasto em combustível foi: R$',tvl1)
