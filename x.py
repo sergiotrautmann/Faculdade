24c90fdbf1db5e9e129e46c83fcbd2f8567943f0
Sergio Patrick Hirayama Trautmann <42048914+sergiotrautmann@users.noreply.github.com>
2018-08-23 21:22:02 -0300

    Update x.py

--- x.py
+++ x.py
@@ -36,8 +36,9 @@ def mdc(n1, n2):
         return n1
     else:
         return mdc (n2, n1 % n2)
-print(mdc(256,454))
-
+n1 = float(input('Digite o primeiro numero:'))
+n2 = float(input('Digite o segundo numero:'))
+print(mdc(n1, n2))
 
 def is_triangle(n1,n2,n3):
     if n1 > n2 + n3:
