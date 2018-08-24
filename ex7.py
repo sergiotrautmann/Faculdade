27f7d48c0f5250c69e09d215e9b954035df40697
Sergio Patrick Hirayama Trautmann <42048914+sergiotrautmann@users.noreply.github.com>
2018-08-23 22:31:20 -0300

    Add files via upload

--- ex7.py
+++ ex7.py
@@ -1,11 +1,14 @@
-arquivo = open('abastecimento.txt','r')
+arquivo = open('abastecimento.txt','r+')
 conteudo = arquivo.readlines()
 km = input('Qual a quilometragem do veiculo na hora do abastecimento?:')
 quantidade = input('Qual a quantidade de combustivel abastecido?:')
 valor = input('Qual o valor pago?:')
-conteudo.append('\n' + km)
-conteudo.append('\n' + quantidade)
-conteudo.append('\n' + valor)
-arquivo = open('abastecimento.txt', 'w')
+conteudo.append('quilometro: \n' + km)
+conteudo.append('\nquantidade: \n' + quantidade)
+conteudo.append('\nvalor: \n'  + valor +'\n')
+arquivo = open('abastecimento.txt', 'r+')
 arquivo.writelines(conteudo)
+
 arquivo.close()
+
+
