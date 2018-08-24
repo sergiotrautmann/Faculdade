adf10c003929e5c7c88bc0d7d68dfc0598de0a09
Sergio Patrick Hirayama Trautmann <42048914+sergiotrautmann@users.noreply.github.com>
2018-08-24 18:42:55 -0300

    Update ex7.py

--- ex7.py
+++ ex7.py
@@ -12,19 +12,19 @@ arquivo.writelines(conteudo)
 arquivo.close()
 
 
-arquivo = open('abastecimento.txt','r+')
+arquivo = open('abastecimento.txt', 'r+')
 conteudo = arquivo.readlines()
 km = input('Qual a quilometragem do veiculo na hora do abastecimento?:')
 quantidade = input('Qual a quantidade de combustivel abastecido?:')
 valor = input('Qual o valor pago?:')
-c = km + ';' + quantidade + ';' + valor +'\n'
+c = km + ';' + quantidade + ';' + valor + '\n'
 conteudo.append(c)
 arquivo = open('abastecimento.txt', 'r+')
-arquivo.writelines(conteudo )
+arquivo.writelines(conteudo)
 arquivo.close()
-arquivo = open('abastecimento.txt','r')
+arquivo = open('abastecimento.txt', 'r')
 for linha in arquivo:
-    l =linha.split(';')
+    l = linha.split(';')
     tkm = float(l[0])
     tqt = float(l[1])
     tvl = float(l[2])
@@ -35,6 +35,9 @@ for linha in arquivo:
     tqt1 += + tqt
     tvl1 += + tvl
     media = 0
-    media += tkm1/ tqt1
-    print('a media de consumo por km é', media)
-    print('o total gasto em combustível foi: R$',tvl1)
+    media += tkm1 / tqt1
+    ckm = (tqt1 / tvl1) / tkm1
+    print('A media de consumo por km é', media)
+    print('O total gasto em combustível foi: R$', tvl1)
+    print('O custo por km foi: R$', ckm)
+arquivo.close()
