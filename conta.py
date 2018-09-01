09948396d5c1424cfa7125dd18edc1556187f9b6
Sergio Patrick Hirayama Trautmann <42048914+sergiotrautmann@users.noreply.github.com>
2018-08-31 22:15:19 -0300

    Add files via upload

--- conta.py
+++ conta.py
@@ -1,5 +1,3 @@
-#criar uma classe conta, com os atributos(numero,agencia,tipo,saldo) e metodos(depósito, saque, transferencia e consultar saldo)
-
 class Conta:
     def __init__(self, numero, agencia, tipo, saldo):
         self.numero = numero
@@ -7,7 +5,7 @@ class Conta:
         self.tipo = tipo
         self.saldo = saldo
 
-    def depósito(self,qtd):
+    def deposito(self,qtd):
         if self.numero & self.agencia:
             self.saldo += qtd
 
@@ -16,21 +14,28 @@ class Conta:
             if self.saldo >= qtd:
                 self.saldo -= qtd
 
-    def transferencia(self,Conta, qtd):
+    def transferencia(self, conta, qtd):
         if self.numero & self.agencia:
             if self.saldo >= qtd:
-                self.saldo -=qtd
-                Conta.saldo += qtd
+                self.saldo -= qtd
+                conta.receber(qtd)
+
+    def receber(self, qtd):
+        if self.numero & self.agencia:
+            if self.saldo >= qtd:
+                self.saldo += qtd
+
 
     def mostrar_saldo(self):
         if self.numero & self.agencia:
-            print('Seu saldo é de: R$', self.saldo)
-
+            return self.saldo
 
 meu_conta = Conta(111, 12 - 1, 'poupança', 100.00)
-meu_conta.depósito(100.00)
+meu_conta.deposito(100.00)
 meu_conta.saque(50.00)
+
 ww_conta = Conta(222, 21-2,'conta corrente', 200.00)
 meu_conta.transferencia(ww_conta, 150.00)
+
 print(meu_conta.saldo, ww_conta.saldo)
-ww_conta.mostrar_saldo()
+print(f'O saldo da conta é: {meu_conta.mostrar_saldo()}')
